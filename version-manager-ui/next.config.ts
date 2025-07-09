3a9b4565fc34604895990b9c461a9ffbd60551a5
Arpit KHare <arpitkhare33@gmail.com>
2025-07-09 15:12:34 +0530

    output export added

--- version-manager-ui/next.config.ts
+++ version-manager-ui/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
+  output: "export",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
